8ecfefba3746203b6399e3ec59110a96013628c7
kazumakano <kazumakano@outlook.jp>
2021-12-25 16:54:00 +0900

    minor modify

--- src/sample.ts
+++ src/sample.ts
@@ -3,10 +3,8 @@ import listAllReposLangs from "./list-all-repos-langs"
 const USER_NAME = "your user name"
 const TOKEN = "your access token"
 
-sample()
-
-async function sample(): Promise<void> {
-  const langDict = await listAllReposLangs(USER_NAME, TOKEN)
-
-  console.table(langDict)
+const sample = async () => {
+  console.table(await listAllReposLangs(USER_NAME, TOKEN))
 }
+
+sample()
