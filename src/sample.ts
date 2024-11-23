ac3b53207e0aff96f7668ccb006cb8b06f25f5c5
kazumakano <kazumakano@outlook.jp>
2024-11-23 23:43:13 +0900

    several updates

--- src/sample.ts
+++ src/sample.ts
@@ -3,8 +3,4 @@ import listAllReposLangs from "./list-all-repos-langs"
 const USER_NAME = "your user name"
 const TOKEN = "your access token"
 
-const sample = async () => {
-  console.table(await listAllReposLangs(USER_NAME, TOKEN))
-}
-
-sample()
+listAllReposLangs(USER_NAME, TOKEN).then(d => console.table(d))
